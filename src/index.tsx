19f2bf7ff6e4d26f464fca51635c183a5753d0ae
ThNam203 <hthnam203@gmail.com>
2024-09-23 21:13:21 +0700

    change env

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import SharingFileScreen from './App';
 import { Toaster } from './components/ui/toaster';
 
+console.log(process.env.REACT_APP_SERVER_URL);
+console.log(process.env.REACT_APP_WS_URL);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
